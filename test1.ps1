cb27c5daf46b0510b7342de27283e9af98842c78
yeungtwh <82325654+yeungtwh@users.noreply.github.com>
2021-09-03 15:34:53 +0800

    Create test1.ps1

---
+++ test1.ps1
@@ -0,0 +1,3 @@
+$env:POWERSHELL_TELEMETRY_OPTOUT = 1
+$env:PATH = "C:\pwsh;.\.azure-kubectl;C:\choco\tools;C:\choco\bin;C:\Git\cmd;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\WINDOWS\system32\config\systemprofile\AppData\Local\Microsoft\WindowsApps;"
+Connect-AzAccount
